28dcfdb306812c210620c75831af70b5c62c3edf
Adolfo Reyes <adolfo0620@gmail.com>
2017-12-07 19:50:43 -0500

    adding coredata

---
+++ goaltracker-app/goaltracker-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goaltracker-app
+//
+//  Created by adolfo reyes on 12/7/17.
+//  Copyright © 2017 adolfo reyes. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType:String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
